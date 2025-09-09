98a842845f700bdc9d196a18952003b12f31f4a2
HAOCHENYE <21724054@zju.edu.cn>
2025-09-09 13:34:53 +0000

    Two space tab for .rst

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -35,3 +35,13 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.softtabstop = 4
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "rst",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 3
+    vim.opt_local.tabstop = 3
+    vim.opt_local.softtabstop = 3
+  end,
+})
