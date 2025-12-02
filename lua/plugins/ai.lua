97d3d33537d2488bc8a261f07dc32db7dc736116
nil0x9 <nil.0x9@proton.me>
2025-12-02 20:13:51 +0800

    enable copilot && switch smear back on

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,7 +1,7 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    enabled = false,
+    enabled = true,
     branch = "canary",
     dependencies = {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
@@ -9,15 +9,12 @@ return {
     },
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
-      debug = true, -- Enable debugging
-      model = 'gpt-5'
-      -- See Configuration section for rest
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
   {
     "zbirenbaum/copilot.lua",
-    enabled = false,
+    enabled = true,
     event = "InsertEnter",
     config = function()
       require("copilot").setup({
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -72,7 +72,7 @@ return {
     },
   {
   "sphamba/smear-cursor.nvim",
-  enabled = false,
+  enabled = true,
   opts = {
       -- Smear cursor when switching buffers or windows.
       smear_between_buffers = true,
