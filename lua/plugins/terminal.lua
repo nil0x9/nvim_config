10d6b73198db575a091feb6ad466dfd4c2fe42db
nil0x9 <nil.0x9@proton.me>
2026-03-18 22:48:32 +0800

    [Feature] add float terminal

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -2,12 +2,52 @@ return {
   {
     "akinsho/toggleterm.nvim",
     version = "*",
+    event = "VeryLazy",
     opts = {
       size = 25,
       close_on_exit = true,
       shell = vim.o.shell,
       auto_scroll = false,
-      open_mapping = '<C-/>',
+      float_opts = {
+        border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        width = function()
+          return math.floor(vim.o.columns * 0.85)
+        end,
+        height = function()
+          return math.floor(vim.o.lines * 0.8)
+        end,
+        winblend = 8,
+        title = " Terminal ",
+        title_pos = "center",
+      },
+      highlights = {
+        FloatBorder = { guifg = "#7aa2f7" },
+        NormalFloat = { guibg = "#1a1b26" },
+        FloatTitle = { guifg = "#c0caf5", guibg = "#1a1b26" },
+      },
+      on_open = function(term)
+        vim.cmd("setlocal nonumber norelativenumber signcolumn=no")
+        vim.cmd("startinsert")
+      end,
     },
+    config = function(_, opts)
+      require("toggleterm").setup(opts)
+
+      local Terminal = require("toggleterm.terminal").Terminal
+
+      local horizontal_term = Terminal:new({
+        direction = "horizontal",
+        hidden = true,
+      })
+
+      local float_term = Terminal:new({
+        direction = "float",
+        hidden = true,
+      })
+
+      local map = vim.keymap.set
+      map({ "n", "t" }, "<C-/>", function() horizontal_term:toggle() end, { desc = "Horizontal terminal" })
+      map({ "n", "t" }, "<M-/>", function() float_term:toggle() end, { desc = "Float terminal" })
+    end,
   },
 }
