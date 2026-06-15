1e5ae9032e7f263e820c46c80a7a70699cdea382
nil0x9 <nil.0x9@proton.me>
2026-06-15 15:18:43 +0800

    disable copilot autofill

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -12,32 +12,32 @@ return {
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
-  {
-    "zbirenbaum/copilot.lua",
-    enabled = true,
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        filetypes = {
-          markdown = true,
-        },
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          keymap = {
-            accept = "<M-l>",
-            accept_word = false,
-            accept_line = false,
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
-          },
-        },
-        -- 设置高亮颜色
-        vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#F0E68C" }),
-      })
-    end,
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   enabled = true,
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("copilot").setup({
+  --       filetypes = {
+  --         markdown = true,
+  --       },
+  --       suggestion = {
+  --         enabled = true,
+  --         auto_trigger = true,
+  --         keymap = {
+  --           accept = "<M-l>",
+  --           accept_word = false,
+  --           accept_line = false,
+  --           next = "<M-]>",
+  --           prev = "<M-[>",
+  --           dismiss = "<C-]>",
+  --         },
+  --       },
+  --       -- 设置高亮颜色
+  --       vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#F0E68C" }),
+  --     })
+  --   end,
+  -- },
   -- 不在 cmp 里体现 ai 提示，只通过 virtual text 里提示
   { "zbirenbaum/copilot-cmp", enabled = false },
   {
